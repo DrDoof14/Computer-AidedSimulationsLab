8a9e99e4d33fd567182b5550c23257ba3b08f897
Ali Ghasemi <ali.ghasemi.pub@gmail.com>
2022-11-02 17:48:10 +0100

    fixing some bugs

--- 06/README.txt.txt
+++ 06/README.txt.txt
@@ -1,3 +1,4 @@
 Just have to run the code on an IDE or a terminal.
 on a terminal, you can write: python lab06.py
 and it will ask you about the distribution you are looking for.
+If you choose the binomial distribution, then you have to choose the values for n and p as well.
--- 06/lab06.py
+++ 06/lab06.py
@@ -2,21 +2,40 @@ import numpy as np
 from math import *
 import matplotlib.pyplot as plt
 import math
+from decimal import Decimal
+# from decimal import *
 
-p=input('For binomial distribution enter 0 and for normal distribution enter 1: ')
-if p=='0':
-    n = 50
-    p = 0.01
+distro=input('For binomial distribution enter 1 and for normal distribution enter 2: ')
+if distro=='1':
+    condition=input("please choose the desired condition:\n 1) n=10  p=0.5\n 2) n=100  p=0.01\n 3) n=1000000   p=0.00001 (time-consuming)\n Your input is: ")
+    if condition=="1":
+        n=10
+        p=0.5
+    elif condition=="2":
+        n=100
+        p=0.01
+    elif condition=="3":
+        n=1000000  
+        p=0.00001
+    else:
+        print("Please enter the right value.")
+        exit()
+
     l= list(range(1,n + 1))
     def logbinom(k,n,p):
-            temp=(math.log(factorial(n))-(math.log(factorial(n-k)+math.log(k))))+ (k*math.log(p))+((n-k)*math.log(1-p));return math.exp(temp);
+        if condition=='3':
+            temp=Decimal(math.log(factorial(n)))-Decimal(math.log(factorial(n-k)))+Decimal((math.log(k)))+ Decimal(k*math.log(p))+Decimal((n-k)*math.log(1-p))
+            #to solve he problem of int being too large to convert to float
+        else:
+            temp=(math.log(factorial(n))-(math.log(factorial(n-k)+math.log(k))))+ (k*math.log(p))+((n-k)*math.log(1-p))
 
-        
+        return math.exp(temp)
+
     dist = [logbinom(k, n, p) for k in l]
 
     plt.bar(l, dist)
     plt.show()
-elif p=='1':
+elif distro=='2':
     f=lambda x: np.exp(-(x**2)/2)/(sqrt(2*3.14))
 
     M=0.3 #scale factor
@@ -36,3 +55,6 @@ elif p=='1':
     v_size=v.size
     efficiency=v_size/u1_size
     print("efficiency factor is equal to {}".format(efficiency))
+else:
+    print("please enter the right value.")
+    exit()
